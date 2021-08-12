139dc90f08b76fb66dd3e5f6843af0b0c0d2e772
joshua-lake <joshua.p.lake@gmail.com>
2021-08-13 10:56:41 +1200

    removed test email

--- client/components/validationHelper.js
+++ client/components/validationHelper.js
@@ -1,5 +1,3 @@
-const testEmail = 'jj@joshiscool.com.com'
-
 export function validateEmail (email) {
   const emailTest = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
   const validation = emailTest.test(email)
